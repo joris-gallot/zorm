3664e802183b06f089daf0a749eefb80c1ee1b36
Joris <joris.gallot18@gmail.com>
2025-04-07 11:22:04 +0200

    test: add tests for query

---
+++ packages/core/tests/query.test.ts
@@ -0,0 +1,33 @@
+import { assertType, describe, expect, it } from 'vitest'
+import { z } from 'zod'
+import { defineEntity, defineQueryBuilder } from '../src/orm'
+
+describe('query', () => {
+  it('should valid types', () => {
+    const User = defineEntity('user', z.object({
+      id: z.number(),
+      name: z.string(),
+    }))
+
+    const query = defineQueryBuilder(User).query()
+
+    assertType<typeof query>(
+      {
+        where: () => query,
+        orWhere: () => query,
+        get: () => [],
+      },
+    )
+  })
+
+  it('should return all users', () => {
+    const User = defineEntity('user', z.object({ id: z.number(), name: z.string() }))
+
+    const userQuery = defineQueryBuilder(User)
+
+    userQuery.save([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }])
+
+    const users = userQuery.query().get()
+    expect(users).toEqual([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }])
+  })
+})
