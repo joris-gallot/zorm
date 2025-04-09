2d8a9d9c6e0e075c6177536b946b4ea8eff2a21b
Joris <joris.gallot18@gmail.com>
2025-04-09 22:10:13 +0200

    fix(vue): lint

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,17 +1,17 @@
 import { defineReactivityAdapter } from '@zorm-ts/core'
 import { shallowRef, triggerRef } from 'vue'
 
-export function useReactivityAdapter() {
+export function useReactivityAdapter(): void {
   return defineReactivityAdapter(
     () => {
       const state = shallowRef(0)
 
       return {
-        depend: () => {
+        depend: (): void => {
           // eslint-disable-next-line ts/no-unused-expressions
           state.value
         },
-        trigger: () => {
+        trigger: (): void => {
           triggerRef(state)
         },
       }
