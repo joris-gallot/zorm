9bb1ce5ffacf4d6638d12f972d197cda52d8d52f
Joris <joris.gallot18@gmail.com>
2025-04-19 16:08:49 +0200

    test(defineQueryBuilder): should have only entity name for relations definition

--- packages/core/tests/defineQueryBuilder.test.ts
+++ packages/core/tests/defineQueryBuilder.test.ts
@@ -3,6 +3,49 @@ import { z } from 'zod'
 import { defineEntity, defineQueryBuilder } from '../src/orm'
 
 describe('defineQueryBuilder', () => {
+  it('should validate relations types', () => {
+    const User = defineEntity('user', z.object({
+      id: z.number(),
+      name: z.string(),
+      isAdmin: z.boolean(),
+      age: z.number().optional(),
+    }))
+
+    const Post = defineEntity('post', z.object({
+      id: z.number(),
+      title: z.string(),
+      userId: z.number(),
+      publishedAt: z.date(),
+      isPublished: z.boolean(),
+    }))
+
+    defineQueryBuilder([User], ({ one }) => ({
+      user: {
+        posts: one(Post, {
+          field: User.fields.id,
+          reference: Post.fields.userId,
+        }),
+      },
+    }))
+
+    defineQueryBuilder([User], ({ one }) => ({
+      user: {
+        posts: one(Post, {
+          field: User.fields.id,
+          reference: Post.fields.userId,
+        }),
+      },
+
+      // TODO: @ts-expect-error foo is not an entity
+      foo: {
+        bar: one(User, {
+          field: User.fields.id,
+          reference: User.fields.id,
+        }),
+      },
+    }))
+  })
+
   it('should validate relations reference types', () => {
     const User = defineEntity('user', z.object({
       id: z.number(),
@@ -17,6 +60,21 @@ describe('defineQueryBuilder', () => {
       userId: z.number(),
       publishedAt: z.date(),
       isPublished: z.boolean(),
+    }))
+
+    defineQueryBuilder([User, Post], ({ many, one }) => ({
+      user: {
+        posts: many(Post, {
+          field: User.fields.id,
+          reference: Post.fields.userId,
+        }),
+      },
+      post: {
+        user: one(User, {
+          field: Post.fields.userId,
+          reference: User.fields.id,
+        }),
+      },
     }))
 
     defineQueryBuilder([User, Post], ({ many, one }) => ({
@@ -54,7 +112,7 @@ describe('defineQueryBuilder', () => {
           // @ts-expect-error must be a string or number
           reference: User.fields.isAdmin,
         }),
-      }
+      },
     }))
   })
 })
