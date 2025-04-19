4bf1b9f7afcbdeea5e65adb3bdc88ebc6a89f428
Joris <joris.gallot18@gmail.com>
2025-04-19 16:57:33 +0200

    fix(lint): indent

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -2,14 +2,12 @@ export type Prettify<T> = {
   [K in keyof T]: T[K]
 } & {}
 
-export type ExactDeep<T, Shape> = T extends Shape
-  ? Shape extends object
-    ? T extends object
-    ? {
-        [K in keyof T]: K extends keyof Shape
-          ? ExactDeep<T[K], Shape[K]>
-          : never
-      } & (Exclude<keyof T, keyof Shape> extends never ? unknown : never)
-      : T
-    : T
+export type ExactDeep<T, Shape> = T extends Shape ? Shape extends object ? T extends object
+  ? {
+    [K in keyof T]: K extends keyof Shape
+      ? ExactDeep<T[K], Shape[K]>
+      : never
+  } & (Exclude<keyof T, keyof Shape> extends never ? unknown : never)
+  : T
+  : T
   : never
