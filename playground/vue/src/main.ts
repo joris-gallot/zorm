9c00eab0d5d06d8ca9e5249b2c60edc4e94322df
Joris <joris.gallot18@gmail.com>
2025-05-03 20:47:55 +0200

    chore(playground): localstorage

--- playground/vue/src/main.ts
+++ playground/vue/src/main.ts
@@ -1,4 +1,3 @@
-import { useReactiveDatabase } from '@zorm-ts/vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { postQuery, userQuery } from './queries.js'
@@ -6,58 +5,60 @@ import './style.css'
 
 const app = createApp(App)
 
-useReactiveDatabase()
+const zDb = JSON.parse(localStorage.getItem('zorm_database') ?? '{}')
 
-postQuery.save([{
-  id: 1,
-  title: 'Post 1',
-  userId: 1,
-  imageId: 1,
-}, {
-  id: 2,
-  title: 'Post 2',
-  userId: 1,
-  imageId: 2,
-}, {
-  id: 3,
-  title: 'Post 3',
-  userId: 2,
-  imageId: 3,
-  user: {
-    id: 2,
-    name: 'Jane',
-    email: 'jane@doe.com',
-    age: 20,
-  },
-}])
-
-userQuery.save([
-  {
+if (!Object.keys(zDb.user || {}).length) {
+  postQuery.save([{
     id: 1,
-    name: 'John',
-    email: 'john@doe.com',
-    age: 10,
-    posts: [
-      {
-        id: 2,
-        title: 'Post 2',
-        userId: 1,
-        imageId: 2,
-      },
-    ],
-  },
-  {
+    title: 'Post 1',
+    userId: 1,
+    imageId: 1,
+  }, {
     id: 2,
-    name: 'Jane',
-    email: 'jane@doe.com',
-    age: 20,
-  },
-  {
+    title: 'Post 2',
+    userId: 1,
+    imageId: 2,
+  }, {
     id: 3,
-    name: 'Jim',
-    email: 'jim@beam.com',
-    age: 30,
-  },
-])
+    title: 'Post 3',
+    userId: 2,
+    imageId: 3,
+    user: {
+      id: 2,
+      name: 'Jane',
+      email: 'jane@doe.com',
+      age: 20,
+    },
+  }])
+
+  userQuery.save([
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@doe.com',
+      age: 10,
+      posts: [
+        {
+          id: 2,
+          title: 'Post 2',
+          userId: 1,
+          imageId: 2,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Jane',
+      email: 'jane@doe.com',
+      age: 20,
+    },
+    {
+      id: 3,
+      name: 'Jim',
+      email: 'jim@beam.com',
+      age: 30,
+    },
+  ])
+}
 
 app.mount('#app')
