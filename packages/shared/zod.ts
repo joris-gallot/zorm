1555b8a96b565d7b0e6be8863e6d7b63b926629d
Joris <joris.gallot18@gmail.com>
2025-11-17 16:33:56 +0100

    chore: add log zod version

--- packages/shared/zod.ts
+++ packages/shared/zod.ts
@@ -1,5 +1,3 @@
 import { z } from 'zod'
 
-const zodVersion = z.core.version.major
-
-export const zodEmail = zodVersion === 4 ? z.email() : z.string().email()
+export const zodEmail = z.string().email()
